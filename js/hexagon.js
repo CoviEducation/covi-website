c79718dee5cb705c0e89f9659630d7576237d56d
Anit <anit.das1@gmail.com>
2015-10-08 21:31:10 -0400

    getting closer with hexmath

--- js/hexagon.js
+++ js/hexagon.js
@@ -2,18 +2,37 @@ $(document).ready(function() {
   // Based on parent div width calculate
   // resize and move hexagons accordingly
 
-  transformHexagons = function(){
-    min_width = 3;
-    widthToHeightRatio = 12;
+  min_width = 3;
+  widthToHeightRatio = 0.8660254037; // h = wthr * w
+
+  transformHexagons = function(divWidth){
+    // Transform divwidth to hexwidth
+    // width of hexwidth = 4a
+    // divwidth = 10a
+    // hexwdith = 4a
+    // hexwidth = .4 divwidth
+    // Subtract margins?
+
+    // should this number scale?
+    mywidth = divwidth - 160;
+
+    hexWidth = 0.4 * mywidth;
+    hexHeight = hexWidth * widthToHeightRatio;
+
+    console.log( " DIV WIDTH (" + divWidth + ") | HEXAGON WIDTH (" + hexWidth + ") | HEXAGON HEIGHT (" + hexHeight + ")")
+    // $(".heximage")
+    //   .width(hexWidth)
+    //   .height(hexHeight);
+
+    $(".tile ")
+      .width(hexWidth)
+      .height(hexHeight);
   }
 
   // Listen to window resizing
   $(window).resize(function(){
-
-    console.log("WINDOW HEIGHT: " + $(window).height());
-    console.log("WINDOW WIDTH " + $(window).width());
-
-    console.log("PARENT DIV HEIGHT: " + $("#hexagon-column").height());
-    console.log("PARENT DIV WIDTH: " + $("#hexagon-column").width());
+    divwidth = $(".hexagons").width();
+    transformHexagons(divwidth);
   });
 });
+
