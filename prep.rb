a07bdc3c6e4bcdefc42d7d287eb787f66362ea66
Anit <anit.das1@gmail.com>
2015-11-25 02:24:32 -0500

    Wishful thinking

---
+++ prep.rb
@@ -0,0 +1,18 @@
+###################
+## Just keep in mind if you're editing index.html or not!!!!
+## While in master, copy index to original_index.html (Manually at first! Eventually Not!)
+## Then in production, take original_index.html, substitute stuff, save as index.html
+
+# Read index.html
+
+# Find substitution index point
+
+# Read all contents
+
+# Copy original_index.html to index.html
+
+
+#
+
+
+
