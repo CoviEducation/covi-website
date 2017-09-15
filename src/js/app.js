2e0fee19bde1e86871b885958b671ff3fa3d804b
Adam Miller <ammiller@linkedin.com>
2017-09-14 17:01:08 -0700

    About page v1

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   let active = document.getElementById(name);
-  function homeRoute(){
+  function homeRoute(e){
     var name = window.location.hash.replace('#', '') || 'home';
     active && active.classList.remove('visible')
     active = document.getElementById(name+'-panel');
-    active && active.classList.add('visible')
+    active && active.classList.add('visible');
+    e && e.preventDefault();
+    e && e.stopPropagation();
   }
   window.addEventListener("hashchange", homeRoute, false);
   homeRoute();
